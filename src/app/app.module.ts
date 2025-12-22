3d38eb9517500e8080466732f0e06262c8b97ca2
tara53cic <tara53cicc@gmail.com>
2025-12-22 23:34:32 +0100

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,53 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
+import {HttpClientModule} from '@angular/common/http';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { LoginComponent } from './login/login.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+
+import {ApiService} from './service/api.service';
+import {AuthService} from './service/auth.service';
+import {ConfigService} from './service/config.service';
+
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { TokenInterceptor } from './interceptor/TokenInterceptor';
+import { AngularMaterialModule } from './angular-material/angular-material.module';
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    LoginComponent,
+    SignUpComponent,
+   
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    AppRoutingModule,
+    NoopAnimationsModule,
+    AngularMaterialModule,
+    FormsModule,
+    ReactiveFormsModule
+  ],
+  providers: [ 
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: TokenInterceptor,
+      multi: true
+    },
+    AuthService,
+    ApiService,
+    ConfigService,
+  ],
+  bootstrap: [AppComponent],
+  schemas: [NO_ERRORS_SCHEMA]
+})
+export class AppModule { }
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import {ConfigService} from '../service/config.service';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+
+ 
+
+  constructor(
+    private config: ConfigService
+  ) {
+  }
+
+  ngOnInit() {
+  }
+
+}
